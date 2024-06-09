6f9a10b325192a6de34d35ced5d2fb969919ae26
Gaby Lentino <86497616+gabylentino@users.noreply.github.com>
2024-06-09 19:11:44 -0300

    Delete literaturaChallenge/literatura directory

--- literaturaChallenge/literatura/src/main/java/com/challengeliteraturaoracleone/literatura/service/IConvierteDatos.java
+++
@@ -1,9 +0,0 @@
-package com.challengeliteraturaoracleone.literatura.service;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-
-public interface IConvierteDatos {
-    <T> T obtenerDatos(String json, Class<T> clase);
-}
